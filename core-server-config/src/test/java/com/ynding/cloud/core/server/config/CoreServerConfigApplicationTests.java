8053f09eca05fe021f4fbf7ba5c8e69d36494e49
yaha <913690560@qq.com>
2019-12-10 11:38:07 +0800

    修改springboot版本和springCloud匹配

--- core-server-config/src/test/java/com/ynding/cloud/core/server/config/CoreServerConfigApplicationTests.java
+++ core-server-config/src/test/java/com/ynding/cloud/core/server/config/CoreServerConfigApplicationTests.java
@@ -1,6 +1,6 @@
 package com.ynding.cloud.core.server.config;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
