02d3b9d4e68e5e2335f7d9d42fba919bbdc5ef73
yaha <913690560@qq.com>
2019-12-31 13:35:21 +0800

    zuul

---
+++ route-server-zuul/src/test/java/com/ynding/cloud/route/zuul/RouteServerZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynding.cloud.route.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteServerZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
