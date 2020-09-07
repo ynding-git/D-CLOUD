69f2de874bf824e34a03ff4cb64795c63feb92c6
yaha <913690560@qq.com>
2020-09-07 16:34:23 +0800

    turbine配置修改修改

--- monitor-turbine/src/main/java/com/ynding/cloud/turbine/MonitorTurbineApplication.java
+++ monitor-turbine/src/main/java/com/ynding/cloud/turbine/MonitorTurbineApplication.java
@@ -3,14 +3,14 @@ package com.ynding.cloud.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author ynding
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableTurbineStream
+@EnableTurbine
 public class MonitorTurbineApplication {
 
     public static void main(String[] args) {
